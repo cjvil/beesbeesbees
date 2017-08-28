81a95dc15b56da651612feeb5722889ae192e9dc
Christie Villanueva <christiejvillanueva@gmail.com>
2017-08-28 11:32:12 -0700

    Add treasureChest property to ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -4,6 +4,7 @@ var ForagerBee = function() {
   this.job = 'find pollen';
   this.color = 'yellow';
   this.canFly = true;
+  this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(Bee.prototype);
