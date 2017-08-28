3e93dbb3fa9db521ffde6bef2e2981a010a3c1d5
Christie Villanueva <christiejvillanueva@gmail.com>
2017-08-28 11:36:43 -0700

    Add unique properties to RetiredForagerBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,10 @@
 var RetiredForagerBee = function() {
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
